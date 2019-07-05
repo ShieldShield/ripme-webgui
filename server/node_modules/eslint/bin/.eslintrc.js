75f74b4cd884ef24b71b73ff1f1a0399bb2da7c2
ShieldShield <jannik14102001@gmail.com>
2019-07-05 23:06:59 +0200

    vue up and running

---
+++ server/node_modules/eslint/bin/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
