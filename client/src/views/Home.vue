812117969b2941225b70c6064acb5d4be33eff43
ShieldShield <jannik14102001@gmail.com>
2019-07-06 20:31:46 +0200

    home now doesn't allows same entries

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,103 +1,119 @@
 <template>
-	<div class="ani-slideInDown row justify-content-center">
-		<div class="col-lg-6">
-			<ToDoInput @eventAddNewTask="onAddNewTask"/>
+  <div class="ani-slideInDown row justify-content-center">
+    <div class="col-lg-6">
+      <ToDoInput @eventAddNewTask="onAddNewTask" />
 
-			<ul class="list mt-3">
-				<ListItem v-for="item in itemList" :key="item.id" :text="item.text" :id="item.id" :isDone="item.isDone" @eventTaskStatusChange="onTaskStatusChange" @eventTaskDelete="onTaskDelete" />
-			</ul>
-		</div>
-	</div>
+      <ul class="list mt-3">
+        <ListItem
+          v-for="item in itemList"
+          :key="item.id"
+          :text="item.text"
+          :id="item.id"
+          :isDone="item.isDone"
+          @eventTaskStatusChange="onTaskStatusChange"
+          @eventTaskDelete="onTaskDelete"
+        />
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-	// @ is an alias to /src
-	import ToDoInput from "@/components/ToDoInput.vue"
-	import ListItem from "@/components/ListItem.vue"
+// @ is an alias to /src
+import ToDoInput from "@/components/ToDoInput.vue";
+import ListItem from "@/components/ListItem.vue";
 
-	export default {
-		name: "home",
-		components: {
-			ToDoInput,
-			ListItem
-		},
-		data() {
-            return {
-                itemList: []
-            }
-        },
-        methods: {
-			/**
-			 * Event: add new task
-			 */
-			onAddNewTask(taskName) {
-				const task = {
-					id: (new Date()).getTime(),
-					text: taskName,
-					isDone: false
-				}
+export default {
+  name: "home",
+  components: {
+    ToDoInput,
+    ListItem
+  },
+  data() {
+    return {
+      itemList: []
+    };
+  },
+  methods: {
+    /**
+     * Event: add new task
+     */
+    onAddNewTask(taskName) {
+      const task = {
+        id: new Date().getTime(),
+        text: taskName,
+        isDone: false
+      };
+      if (!contains(this.itemList, "text", taskName)) {
+          this.itemList.push(task);
+      }
+    },
 
-				this.itemList.push(task)
-			},
+    /**
+     * Event: on task status changed
+     */
+    onTaskStatusChange(id, checked) {
+      console.log(id, checked);
 
-            /**
-             * Event: on task status changed
-             */
-            onTaskStatusChange(id, checked) {
-                console.log(id, checked)
+      let item = this.itemList.find(i => i.id == id);
+      if (item) {
+        item.isDone = checked;
+      }
 
-                let item = this.itemList.find(i => i.id == id)
-                if (item) {
-                    item.isDone = checked
-                }
-                
-                console.log(this.itemList)
-            },
+      console.log(this.itemList);
+    },
 
-            /**
-             * Event: on task deleted
-             */
-            onTaskDelete(id) {
-                console.log(id)
+    /**
+     * Event: on task deleted
+     */
+    onTaskDelete(id) {
+      console.log(id);
 
-                let index = this.itemList.findIndex(i => i.id == id)
-                if (index > -1) {
-                    this.itemList.splice(index, 1)
-                }
+      let index = this.itemList.findIndex(i => i.id == id);
+      if (index > -1) {
+        this.itemList.splice(index, 1);
+      }
 
-                console.log(this.itemList)
-            },
+      console.log(this.itemList);
+    },
 
-            /**
-             * Load item list from local storage
-             */
-            loadItemList() {
-                this.itemList = JSON.parse(localStorage.getItem("VuejsTodo")) || []
+    /**
+     * Load item list from local storage
+     */
+    loadItemList() {
+      this.itemList = JSON.parse(localStorage.getItem("VuejsTodo")) || [];
 
-                console.log("this.itemList =", this.itemList)
-            },
+      console.log("this.itemList =", this.itemList);
+    },
 
-            /**
-             * Update the item list to local storage
-             */
-            updateItemList() {
-                localStorage.setItem("VuejsTodo", JSON.stringify(this.itemList))
-            }
-        },
-        mounted() {
-            // Load item list from local storage
-            this.loadItemList()
-        },
-        watch: {
-            itemList: {
-                handler(value) {
-                    console.log("item changed")
+    /**
+     * Update the item list to local storage
+     */
+    updateItemList() {
+      localStorage.setItem("VuejsTodo", JSON.stringify(this.itemList));
+    }
+  },
+  mounted() {
+    // Load item list from local storage
+    this.loadItemList();
+  },
+  watch: {
+    itemList: {
+      handler(value) {
+        console.log("item changed");
 
-                    // save to localStorage
-                    this.updateItemList()
-                },
-                deep: true
-            }
-        }
-	}
+        // save to localStorage
+        this.updateItemList();
+      },
+      deep: true
+    }
+  }
+};
+
+function contains(arr, key, val) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i][key] === val) return true;
+  }
+  return false;
+}
 </script>
