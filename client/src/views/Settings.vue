3921e8dcf51551fa7268ab03895e6bf9f58e502a
ShieldShield <jannik14102001@gmail.com>
2019-07-08 17:20:46 +0200

    basic settings design

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -6,11 +6,50 @@
       <input type="text" placeholder="Absolute Path" v-model="text" required />
     </form>
     <br />
-    <toggle-button
-      :value="true"
-      :labels="{checked: 'on', unchecked: 'off'}"
-      @change="onChangeEventHandler"
-    />
+    <div class="switches">
+      <table style="text-align:left; margin-left: auto; margin-right: auto;">
+        <tr>
+          <td>Keep saveorder</td>
+          <td>
+            <toggle-button
+              :value="true"
+              :labels="{checked: 'on', unchecked: 'off'}"
+              @change="onChangeEventHandler"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Skip 404 errors</td>
+          <td>
+            <toggle-button
+              :value="true"
+              :labels="{checked: 'on', unchecked: 'off'}"
+              @change="onChangeEventHandler"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>create prop file</td>
+          <td>
+            <toggle-button
+              :value="true"
+              :labels="{checked: 'on', unchecked: 'off'}"
+              @change="onChangeEventHandler"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Rerip</td>
+          <td>
+            <toggle-button
+              :value="true"
+              :labels="{checked: 'on', unchecked: 'off'}"
+              @change="onChangeEventHandler"
+            />
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
